b8d6d0832795cbb5146457ea10f633bd4dd01089
Titan <titanbolics@gmail.com>
2025-12-15 17:40:34 +0100

    feature: add posthog tracking

--- next.config.js
+++ next.config.js
@@ -67,6 +67,24 @@ const nextConfig = {
         : []),
     ],
   },
+  async rewrites() {
+    return [
+      {
+        source: "/ingest/static/:path*",
+        destination: "https://us-assets.i.posthog.com/static/:path*",
+      },
+      {
+        source: "/us/ingest/:path*",
+        destination: "https://us.i.posthog.com/:path*",
+      },
+      {
+        source: "/ingest/:path*",
+        destination: "https://us.i.posthog.com/:path*",
+      },
+    ]
+  },
+  // This is required to support PostHog trailing slash API requests
+  skipTrailingSlashRedirect: true,
 }
 
 module.exports = nextConfig
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import { getBaseURL } from "@lib/util/env"
 import { Metadata } from "next"
+import { PostHogProvider } from "./providers"
 import "styles/globals.css"
 
 export const metadata: Metadata = {
@@ -10,7 +11,9 @@ export default function RootLayout(props: { children: React.ReactNode }) {
   return (
     <html lang="en" data-mode="light">
       <body>
-        <main className="relative">{props.children}</main>
+        <PostHogProvider>
+          <main className="relative">{props.children}</main>
+        </PostHogProvider>
       </body>
     </html>
   )
