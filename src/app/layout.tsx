cc57c37058bf464cdf1a38ef1f31bed4f56c2b22
Titan <titanbolics@gmail.com>
2025-12-28 16:50:47 +0100

    fix: add PostHogProvider to layout and remove Google Analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import { getBaseURL } from "@lib/util/env"
 import { Metadata } from "next"
-import Script from "next/script"
+import { PostHogProvider } from "./providers/posthog-provider"
 import "styles/globals.css"
 
 export const metadata: Metadata = {
@@ -13,30 +13,12 @@ export default function RootLayout({
 }: {
   children: React.ReactNode
 }) {
-  const gaId = process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID
-
   return (
     <html lang="en" data-mode="light">
       <body>
-        <main className="relative">{children}</main>
-
-        {/* Google Analytics */}
-        {gaId && (
-          <>
-            <Script
-              async
-              src={`https://www.googletagmanager.com/gtag/js?id=${gaId}`}
-            />
-            <Script id="google-analytics" strategy="afterInteractive">
-              {`
-                window.dataLayer = window.dataLayer || [];
-                function gtag(){dataLayer.push(arguments);}
-                gtag('js', new Date());
-                gtag('config', '${gaId}');
-              `}
-            </Script>
-          </>
-        )}
+        <PostHogProvider>
+          <main className="relative">{children}</main>
+        </PostHogProvider>
       </body>
     </html>
   )
